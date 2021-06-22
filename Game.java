d948303fbe2d9fe67b00a216d03de09fbae7efd8
rayendito <ryanditod@gmail.com>
2021-06-22 13:20:30 +0700

    improvements

--- Game.java
+++ Game.java
@@ -1,5 +1,5 @@
 class Game{
-    private char[][] grid = {{'1','2','3'},{'4','5','6'},{'7','8','9'}};;
+    private char[] grid = {'1','2','3','4','5','6','7','8','9'};
     
     Game(){
         //initialize array list into 3x3 grid for tic tac toe-ing
@@ -8,11 +8,11 @@ class Game{
     public void printGrid(){
         //hardcoded bc static lmao
         System.out.println("┏━━━┳━━━┳━━━┓");
-        System.out.printf("┃ %c ┃ %c ┃ %c ┃%n",grid[0][0],grid[0][1],grid[0][2]);
+        System.out.printf("┃ %c ┃ %c ┃ %c ┃%n",grid[0],grid[1],grid[2]);
         System.out.println("┣━━━╋━━━╋━━━┫");
-        System.out.printf("┃ %c ┃ %c ┃ %c ┃%n",grid[1][0],grid[1][1],grid[1][2]);
+        System.out.printf("┃ %c ┃ %c ┃ %c ┃%n",grid[3],grid[4],grid[5]);
         System.out.println("┣━━━╋━━━╋━━━┫");
-        System.out.printf("┃ %c ┃ %c ┃ %c ┃%n",grid[2][0],grid[2][1],grid[2][2]);
+        System.out.printf("┃ %c ┃ %c ┃ %c ┃%n",grid[6],grid[7],grid[8]);
         System.out.println("┗━━━┻━━━┻━━━┛");
     }
     public static void main(String[] args) {
